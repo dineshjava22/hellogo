dc88e939490c53312f5b7dd6819179d35033f526
dineshjava22 <java.dinesh22@gmail.com>
2016-01-05 12:55:24 -0500

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
-	 fmt.Fprintf(w, "<h1>DEV BRANCH123</h1>\n")
+	 fmt.Fprintf(w, "<h1>Connector BRANCH123</h1>\n")
     fmt.Fprintf(w, "<h1>Hello from Cisco Shipped!</h1>\n")
 	fmt.Fprintf(w, "<h1>Hello from Dinesh ..!</h1>\n")
 }
